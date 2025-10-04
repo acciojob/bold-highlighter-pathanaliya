79124ee8045560bbd177cb653dc20b6dbe38b5db
Pathan Aliya <144582876+pathanaliya@users.noreply.github.com>
2025-10-04 11:45:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 function highlight() {
     //Write your code here
-
+ // select all <strong> elements
+    let boldWords = document.querySelectorAll('strong');
+    // loop through each and change color to green
+    boldWords.forEach(function(word) {
+        word.style.color = "rgb(0, 128, 0)";
+    });
 
 }
 
 
 function return_normal() {
     //Write your code here
-
+ // select all <strong> elements
+    let boldWords = document.querySelectorAll('strong');
+    // loop through each and change color to green
+    boldWords.forEach(function(word) {
+        word.style.color = "rgb(0, 128, 0)";
+    });
     
 }
