bced9c1b9b3a08b1f199ef1575701e7bcda6a905
Pathan Aliya <144582876+pathanaliya@users.noreply.github.com>
2025-10-04 11:48:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,14 @@
 function highlight() {
-    //Write your code here
- // select all <strong> elements
     let boldWords = document.querySelectorAll('strong');
-    // loop through each and change color to green
     boldWords.forEach(function(word) {
-        word.style.color = "rgb(0, 128, 0)";
+        word.style.color = "rgb(0, 128, 0)"; // green
     });
-
 }
 
-
 function return_normal() {
-    //Write your code here
- // select all <strong> elements
     let boldWords = document.querySelectorAll('strong');
-    // loop through each and change color to green
     boldWords.forEach(function(word) {
-        word.style.color = "rgb(0, 128, 0)";
+        // explicitly reset to rgb(0,0,0) so Cypress sees the exact value
+        word.style.color = "rgb(0, 0, 0)";
     });
-    
 }
